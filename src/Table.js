6091d18577afe18eae35d37b6514dc455b75c0a7
annika <annikaslund@gmail.com>
2019-03-25 11:24:51 -0700

    Add componentDidMount() api call for Table Component

--- src/Table.js
+++ src/Table.js
@@ -3,9 +3,27 @@ import axios from "axios";
 import Card from "./Card";
 
 class Table extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            deckId: "",
+            card: []
+        }
+
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    async componentDidMount(){
+        let result = await axios.get("https://deckofcardsapi.com/api/deck/new/")
+        this.setState({
+            deckId: result.data.deck_id
+        })
+    }
+
     render(){
         return (
             <div className="Table">
+                <button>Gimme a card!</button>
                 <Card />
             </div>
         );
